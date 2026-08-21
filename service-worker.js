fe8e09527ef299205ff573cd1fa77aec2b00c09f
alazarcorreo0-crypto <alazarcorreo0@gmail.com>
2026-08-21 16:43:13 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'presupuesto-v1';
+const urlsToCache = [
+    'index.html',
+    'presupuesto.html',
+    'transacciones.html',
+    'dashboard.html',
+    'patrimonio.html',
+    'configuracion.html',
+    'styles.css',
+    'app.js',
+    'manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
